0a892c3503c1c5ab5318eb01720fb570bf0608ec
俞樾珲 <m18758428901@163.com>
2026-04-15 01:20:06 +0800

    feat: blog 更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://lvyouyou.dev',
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
